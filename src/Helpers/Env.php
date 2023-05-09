229de64ab0c13bf036e6ab653ccb948d7b9d1d5c
thiiagoms <thiagom.devsec@gmail.com>
2023-05-08 22:56:03 -0300

    feat: create end reader :sparkles:

---
+++ src/Helpers/Env.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Narcissus\Helpers;
+
+use Dotenv\Dotenv;
+
+/**
+ * Env file helper
+ *
+ * @package Narcissus\Helpers
+ * @author  Thiago <thiiagoms@proton.me>
+ * @version 1.0
+ */
+final class Env
+{
+    /**
+     * The path to the directory containing the .env file.
+     *
+     * @var string
+     */
+    private const ENVPATH = __DIR__ . '/../../';
+
+    /**
+     * Load env values
+     *
+     * @return void
+     */
+    public static function loadEnv(): void
+    {
+        (Dotenv::createImmutable(self::ENVPATH))->load();
+    }
+}
